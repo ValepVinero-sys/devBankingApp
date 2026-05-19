05f9ea830947c33f6f421883831c2fbbc01015bd
ValepVinero-sys <pasha.orevin@gmail.com>
2026-05-19 20:09:20 +0300

    Update TransferRequest.java

--- src/main/java/com/Bank/TransferRequest.java
+++ src/main/java/com/Bank/TransferRequest.java
@@ -5,16 +5,46 @@ import jakarta.validation.constraints.NotNull;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import java.math.BigDecimal;
+
+/** DTO (data transfer object) for request transfer of funds
+ * <p>
+ *     Used at {@link TransferService#transferMoney(TransferRequest)}
+ *     for data transfer from controller to service.
+ * </p>
+ *
+ * @author Valep Vinreo
+ * @version 1.00
+ * @see TransferService
+ * @since 04-2026
+ *
+ */
 @EqualsAndHashCode(callSuper = false)
 @Data
 public class TransferRequest {
-        @NotBlank(message = "Bill number of sender is required ")
+        /**
+         * Required sender's account number.
+         * Cannot be null.
+         */
+        @NotBlank(message = "Account number of sender is required ")
         private String fromAccountNumber;
-        @NotBlank(message = "Bill number of reciver is required")
+        /**
+         * Required receiver account number.
+         * Cannot be null.
+         */
+        @NotBlank(message = "Bill number of receiver is required")
         private String toAccountNumber;
-        @NotNull(message = "amount reqired")
-        @DecimalMin(value = "0.01", message = "amount should be higher than 0")
+        /**
+         * Required amount of funds.
+         * Cannot be null.
+         */
+        @NotNull(message = "Amount required")
+        @DecimalMin(value = "0.01", message = "Amount should be higher than 0")
         private BigDecimal amount;
         public BigDecimal get_amount(){return amount;}
+
+        /**
+         * Description purpose of transfer (optional).
+         * Can be seen at transfer history.
+         */
         private String description;
     }
