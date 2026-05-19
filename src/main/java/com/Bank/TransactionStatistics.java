6a5e51b23850ed94ffa9051ad651b90c5c15549d
ValepVinero-sys <pasha.orevin@gmail.com>
2026-05-19 20:05:11 +0300

    Update TransactionStatistics.java

--- src/main/java/com/Bank/TransactionStatistics.java
+++ src/main/java/com/Bank/TransactionStatistics.java
@@ -1,4 +1,30 @@
 package com.Bank;
-
+import lombok.Builder;
+import lombok.Data;
+import java.math.BigDecimal;
+/**
+ * DTO for statistic of transaction of all accounts.
+ *
+ * @author Valep Vinreo
+ * @version 1.0
+ * @since 04-2026
+ */
+@Data
+@Builder
 public class TransactionStatistics {
+
+    /** Overall sum of outgoing transfer */
+    private BigDecimal totalOutgoing;
+
+    /** Overall sum of incoming transfer */
+    private BigDecimal totalIncoming;
+
+    /** Overall transaction count */
+    private Long transactionCount;
+
+    /** Average sum of outgoing transfer */
+    private BigDecimal averageOutgoing;
+
+    /** Average sum of incoming transfer */
+    private BigDecimal averageIncoming;
 }
